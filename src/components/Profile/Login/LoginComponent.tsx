b1312bfecdcbeba61f5f3b23c406d9e7f4c602d8
Veselin Popov <veselinpopov@livewallgroup.com>
2021-12-07 22:52:45 +0100

    added login component

--- src/components/Profile/Login/LoginComponent.tsx
+++ src/components/Profile/Login/LoginComponent.tsx
@@ -1,10 +1,54 @@
-import './LoginComponent.css';
-
-const LoginComponent: React.FC<{}> = ({ }) => {
+import {
+  IonBadge,
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonText,
+} from "@ionic/react";
+import { useForm } from "react-hook-form";
+import "./LoginComponent.css";
+import logo from "./logo.png";
+const LoginComponent: React.FC<{}> = ({}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => {
+    console.log("Login data:", data);
+  };
   return (
-    <div className="container">
-      <strong>Login</strong>
-      <p>This is login component</p>
+    <div className="ion-padding">
+      <IonImg src={logo} className="center" />
+      <h3>Welcome to PartiRecept</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <IonItem>
+          <IonLabel position="stacked">E-mail address</IonLabel>
+          <IonInput {...register("email", { required: true })} />
+        </IonItem>
+        {errors.email && <IonText color="danger">Invalid email</IonText>}
+
+        <IonItem>
+          <IonLabel position="stacked">Your password</IonLabel>
+          <IonInput {...register("password")} />
+        </IonItem>
+        <IonButton expand="block" type="submit">
+          Login
+        </IonButton>
+      </form>
+      {/* To do: error handling after submit */}
+      <IonText>
+        <p>or register</p>
+      </IonText>
+      <IonButton expand="block" type="submit">
+        Register
+      </IonButton>
     </div>
   );
 };
