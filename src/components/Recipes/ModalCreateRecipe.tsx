bc6dd974283a4ecb84655beec876c0a9b038c746
Veselin Popov <veselinpopov@livewallgroup.com>
2022-01-12 22:33:00 +0100

    upload images to cloudinary

--- src/components/Recipes/ModalCreateRecipe.tsx
+++ src/components/Recipes/ModalCreateRecipe.tsx
@@ -1,191 +1,243 @@
 import {
-	IonButton,
-	IonContent,
-	IonGrid,
-	IonRow,
-	IonItem,
-	IonLabel,
-	IonInput,
-	IonSelect,
-	IonSelectOption,
-	IonCol,
-	IonIcon,
-	IonItemDivider,
-	IonTextarea,
-} from '@ionic/react';
-import React, { Dispatch, SetStateAction, useContext, useRef, useState } from 'react';
-import axios from 'axios';
-import { cloudUploadOutline } from 'ionicons/icons';
-import { useForm } from 'react-hook-form';
-import './ModalCreateRecipe.css';
-import AppContext from '../../store/AppContext';
+  IonButton,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonCol,
+  IonIcon,
+  IonItemDivider,
+  IonTextarea,
+  IonImg,
+} from "@ionic/react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useRef,
+  useState,
+} from "react";
+import axios from "axios";
+import { cloudUploadOutline } from "ionicons/icons";
+import { useForm } from "react-hook-form";
+import "./ModalCreateRecipe.css";
+import AppContext from "../../store/AppContext";
+import { Console } from "console";
 
 const ModalCreateRecipe: React.FC<{
-	showRecipeCreateModal: boolean;
-	setShowRecipeCreateModal: Dispatch<SetStateAction<boolean>>;
+  showRecipeCreateModal: boolean;
+  setShowRecipeCreateModal: Dispatch<SetStateAction<boolean>>;
 }> = (props) => {
-	const [image, setImage] = useState('');
-	const appContext = useContext(AppContext);
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
-	const fileInput = useRef<HTMLInputElement>(null);
-	const imageSelectedHandler = (event: any) => {
-		const imageURL: any = URL.createObjectURL(event.target.files[0]);
-		setImage(imageURL);
-	};
-	const handleRef = () => {
-		fileInput.current?.click();
-	};
+  const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [imagePath, setimagePath] = useState("");
+  const appContext = useContext(AppContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const fileInput = useRef<HTMLInputElement>(null);
 
-	const onSubmit = (data: any) => {
-		data = {
-			...data,
-			difficulty: parseInt(data.difficulty),
-			numberOfServings: parseInt(data.numberOfServings),
-			preparationTimeTicks: parseInt(data.preparationTimeTicks),
-			ingredients: [],
-			imagePath: 'http://thefountains.ae/wp-content/gallery/dishdash/30B0496-1.jpg',
+  //Select the image from the file input
+
+  const imageSelectedHandler = (file: any) => {
+    const imageURL: any = URL.createObjectURL(file);
+    setImage(file);
+    setImageUrl(imageURL);
+    console.log(file);
+    uploadImage();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "fan6fnua");
+    axios
+      .post("https://api.cloudinary.com/v1_1/dafrxyo42/image/upload", formData)
+      .then((data) => {
+        console.log(data);
+        setimagePath(data.data.url);
+        console.log(imagePath);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setImage("");
+  };
+  //Upload selected image
+  const uploadImage = () => {};
+  const handleRef = () => {
+    fileInput.current?.click();
+  };
+
+  //Submit POST request to API
+
+  const onSubmit = (data: any) => {
+    data = {
+      ...data,
+      difficulty: parseInt(data.difficulty),
+      numberOfServings: parseInt(data.numberOfServings),
+      preparationTimeTicks: parseInt(data.preparationTimeTicks),
+      ingredients: [],
+      imagePath: "test",
+      longitude: 0,
+      latitude: 0,
       rating: 0,
-		};
-		console.log('creating new recipe with data:', data);
+    };
+    console.log("creating new recipe with data:", data);
 
-		axios
-			.post(appContext.http + 'Recipe', data, {headers: {'x-auth': appContext.user?.JWTToken == undefined? '' : appContext.user.JWTToken}})
-			.then(function (response) {
-				console.log(response);
-				props.setShowRecipeCreateModal(false);
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	};
+    axios
+      .post(appContext.http + "Recipe", data, {
+        headers: {
+          "x-auth":
+            appContext.user?.JWTToken == undefined
+              ? ""
+              : appContext.user.JWTToken,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        props.setShowRecipeCreateModal(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	return (
-		<IonContent className='ion-padding'>
-			<h3 className='ion-padding'>Add Recipe</h3>
-			{/* form */}
-			<form onSubmit={handleSubmit(onSubmit)}>
-				{/* <img src={image} /> //Display Selected Image*/}
-				<IonGrid>
-					<IonRow>
-						<IonCol size='4' style={{ margin: 'auto' }}>
-							<IonButton onClick={handleRef} style={{ width: '100%' }}>
-								<IonIcon slot='start' icon={cloudUploadOutline} />
-								Choose Image
-								<input
-									hidden
-									type='file'
-									accept='image/*'
-									{...register('imagePath')}
-									ref={fileInput}
-								/>
-							</IonButton>
-						</IonCol>
-						<IonCol size='8'>
-							<IonItem>
-								<IonLabel position='stacked'>Name of Dish</IonLabel>
-								<IonInput
-									autocomplete='off'
-									required={true}
-									{...register('title')}
-								/>
-							</IonItem>
-							{
-								<IonItem>
-									<IonLabel position='stacked'>Time to cook</IonLabel>
-									<IonInput {...register('preparationTimeTicks')} />
-								</IonItem>
-							}
-							<IonItem>
-								<IonLabel position='stacked'>Shared by</IonLabel>
-								<IonInput autocomplete='off' {...register('sharedBy')} />
-							</IonItem>
+  return (
+    <IonContent className="ion-padding">
+      <h3 className="ion-padding">Add Recipe</h3>
+      {/* form */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* <img src={image} /> //Display Selected Image*/}
+        <IonGrid>
+          <IonRow>
+            <IonCol size="4" style={{ margin: "auto" }}>
+              <IonButton onClick={handleRef} style={{ width: "100%" }}>
+                <IonIcon slot="start" icon={cloudUploadOutline} />
+                Choose Image
+                <input
+                  hidden
+                  type="file"
+                  accept="image/*"
+                  {...register("imagePath")}
+                  ref={fileInput}
+                  onChange={(event: any) => {
+                    imageSelectedHandler(event.target.files[0]);
+                  }}
+                />
+              </IonButton>
+              <IonImg src={imageUrl}></IonImg>
+            </IonCol>
+            <IonCol size="8">
+              <IonItem>
+                <IonLabel position="stacked">Name of Dish</IonLabel>
+                <IonInput
+                  autocomplete="off"
+                  required={true}
+                  {...register("title")}
+                />
+              </IonItem>
+              {
+                <IonItem>
+                  <IonLabel position="stacked">Time to cook</IonLabel>
+                  <IonInput {...register("preparationTimeTicks")} />
+                </IonItem>
+              }
+              <IonItem>
+                <IonLabel position="stacked">Shared by</IonLabel>
+                <IonInput autocomplete="off" {...register("sharedBy")} />
+              </IonItem>
 
-							<IonItem>
-								<IonLabel position='stacked'>Type of cuisine</IonLabel>
-								<IonSelect
-									{...register('type')}
-									cancelText='Cancel'
-									okText='Add'>
-									<IonSelectOption value='Turkish'>Turkish</IonSelectOption>
-									<IonSelectOption value='Mexican'>Mexican</IonSelectOption>
-									<IonSelectOption value='Italian'>Italian</IonSelectOption>
-								</IonSelect>
-							</IonItem>
-						</IonCol>
-					</IonRow>
-				</IonGrid>
-				<IonGrid>
-					<h3 className='ion-padding'>More Details</h3>
-					<IonItem>
-						<IonLabel position='stacked'>Difficulty</IonLabel>
-						<IonSelect
-							{...register('difficulty')}
-							cancelText='Cancel'
-							okText='Add'>
-							<IonSelectOption value={0}>Beginner</IonSelectOption>
-							<IonSelectOption value={1}>Intermediate</IonSelectOption>
-							<IonSelectOption value={2}>Advanced</IonSelectOption>
-						</IonSelect>
-					</IonItem>
-					<IonItem>
-						<IonLabel position='stacked'>Country of origin</IonLabel>
-						<IonInput autocomplete='off' {...register('countryOfOrigin')} />
-					</IonItem>
-					<IonItem>
-						<IonLabel position='stacked'>Number of servings</IonLabel>
-						<IonInput
-							autocomplete='off'
-							type='number'
-							{...register('numberOfServings')}
-						/>
-					</IonItem>
-					<IonItem>
-						<IonLabel position='stacked'>Preparation Time</IonLabel>
-						<IonInput
-							autocomplete='off'
-							{...register('preparationTimeTicks')}
-							type='number'
-						/>
-					</IonItem>
-					<IonItem>
-						<IonLabel position='stacked'>Ingredients</IonLabel>
-						<IonSelect
-							multiple={true}
-							{...register('unlistedIngredients')}
-							cancelText='Cancel'
-							okText='Add'>
-							<IonSelectOption value='tomato'>Tomato</IonSelectOption>
-							<IonSelectOption value='egg'>Egg</IonSelectOption>
-							<IonSelectOption value='butter'>Butter</IonSelectOption>
-						</IonSelect>
-					</IonItem>
-					<IonItemDivider />
-					<h3 className='ion-padding'>Instructions</h3>
-					<IonItem>
-						<IonTextarea
-							placeholder='Add instructions here...'
-							{...register('instructions')}></IonTextarea>
-					</IonItem>
-				</IonGrid>
-				<IonGrid className='ion-padding'>
-					<IonRow class='ion-justify-content-around'>
-						<IonButton type='submit'>Add Recipe</IonButton>
-						<IonButton
-							onClick={() => props.setShowRecipeCreateModal(false)}
-							fill='outline'
-							color='medium'>
-							Close
-						</IonButton>
-					</IonRow>
-				</IonGrid>
-			</form>
-			{/* End form */}
-		</IonContent>
-	);
+              <IonItem>
+                <IonLabel position="stacked">Type of cuisine</IonLabel>
+                <IonSelect
+                  {...register("type")}
+                  cancelText="Cancel"
+                  okText="Add"
+                >
+                  <IonSelectOption value="Turkish">Turkish</IonSelectOption>
+                  <IonSelectOption value="Mexican">Mexican</IonSelectOption>
+                  <IonSelectOption value="Italian">Italian</IonSelectOption>
+                </IonSelect>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <IonGrid>
+          <h3 className="ion-padding">More Details</h3>
+          <IonItem>
+            <IonLabel position="stacked">Difficulty</IonLabel>
+            <IonSelect
+              {...register("difficulty")}
+              cancelText="Cancel"
+              okText="Add"
+            >
+              <IonSelectOption value={0}>Beginner</IonSelectOption>
+              <IonSelectOption value={1}>Intermediate</IonSelectOption>
+              <IonSelectOption value={2}>Advanced</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Country of origin</IonLabel>
+            <IonInput autocomplete="off" {...register("countryOfOrigin")} />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Number of servings</IonLabel>
+            <IonInput
+              autocomplete="off"
+              type="number"
+              {...register("numberOfServings")}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Preparation Time</IonLabel>
+            <IonInput
+              autocomplete="off"
+              {...register("preparationTimeTicks")}
+              type="number"
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Ingredients</IonLabel>
+            <IonSelect
+              multiple={true}
+              {...register("unlistedIngredients")}
+              cancelText="Cancel"
+              okText="Add"
+            >
+              <IonSelectOption value="tomato">Tomato</IonSelectOption>
+              <IonSelectOption value="egg">Egg</IonSelectOption>
+              <IonSelectOption value="butter">Butter</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItemDivider />
+          <h3 className="ion-padding">Instructions</h3>
+          <IonItem>
+            <IonTextarea
+              placeholder="Add instructions here..."
+              {...register("instructions")}
+            ></IonTextarea>
+          </IonItem>
+        </IonGrid>
+        <IonGrid className="ion-padding">
+          <IonRow class="ion-justify-content-around">
+            <IonButton type="submit">Add Recipe</IonButton>
+            <IonButton
+              onClick={() => props.setShowRecipeCreateModal(false)}
+              fill="outline"
+              color="medium"
+            >
+              Close
+            </IonButton>
+          </IonRow>
+        </IonGrid>
+      </form>
+      {/* End form */}
+    </IonContent>
+  );
 };
 
 export default ModalCreateRecipe;
